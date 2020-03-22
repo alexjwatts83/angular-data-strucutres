4979e6954076582cef79b5436102ab3ea9a4efa0
alexwatts83 <alexwatts83@gmail.com>
2020-03-22 18:16:09 +1100

    more bst stuff, putting on hold going back to start

--- src/app/binary-search-trees/binary-search-tree.component.ts
+++ src/app/binary-search-trees/binary-search-tree.component.ts
@@ -12,7 +12,10 @@ export class BinarySearchTreeComponent implements OnInit {
   message: string;
   constructor() { 
     this.tree = new BinarySearchTree<number>();
-    this.inputValue = '99';
+    const values = [43,41,60,48,75,67,99,80];
+    for(let i = 0; i < values.length; i++) {
+      this.tree.insert(values[i]);
+    }
   }
 
   ngOnInit(): void {
@@ -27,5 +30,13 @@ export class BinarySearchTreeComponent implements OnInit {
     const nodeValue = parseInt(this.inputValue);
     const exists = this.tree.lookup(nodeValue);
     this.message = `Value ${nodeValue} ${exists ? ' does exist :)' : ' does not exists :('}`;
+  }
+
+  traverseTree(node: any) {
+    let tree: any;
+    tree = { value: node.value };
+    tree.left = node.left === null ? null : this.traverseTree(node.left);
+    tree.right = node.right === null ? null : this.traverseTree(node.right);
+    return tree;
   }
 }
