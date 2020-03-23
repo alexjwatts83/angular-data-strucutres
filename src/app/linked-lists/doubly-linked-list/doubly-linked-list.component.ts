e37ee8e50ded01f76e12c4bbd0071d8bd98f8914
alexwatts83 <alexwatts83@gmail.com>
2020-03-23 23:42:06 +1100

    added buttons to add random values to linked lists

--- src/app/linked-lists/doubly-linked-list/doubly-linked-list.component.ts
+++ src/app/linked-lists/doubly-linked-list/doubly-linked-list.component.ts
@@ -11,13 +11,20 @@ export class DoublyLinkedListComponent implements OnInit {
   initialValue: number;
   inputValue: number;
   message: string;
+  max: number;
+
   constructor() { 
     this.initialValue = 10;
     this.linkedList = new DoublyLinkedList<number>(this.initialValue);
     this.inputValue = this.initialValue;
+    this.max = 1000;
   }
 
   ngOnInit(): void {
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
   append(): void {
@@ -25,8 +32,20 @@ export class DoublyLinkedListComponent implements OnInit {
     this.message = `Append '${this.inputValue}'`;
   }
 
+  appendRandom(): void {
+    this.inputValue = this.getRandomInt(this.max);
+    this.linkedList.append(this.inputValue);
+    this.message = `Append Random number '${this.inputValue}' between ${this.max}`;
+  }
+
   prepend(): void {
     this.linkedList.prepend(this.inputValue);
     this.message = `Prepend '${this.inputValue}'`;
+  }
+
+  prependRandom(): void {
+    this.inputValue = this.getRandomInt(this.max);
+    this.linkedList.prepend(this.inputValue);
+    this.message = `Prepend Random number '${this.inputValue}' between ${this.max}`;
   }
 }
--- src/app/linked-lists/singly-linked-lists/singly-linked-list.component.ts
+++ src/app/linked-lists/singly-linked-lists/singly-linked-list.component.ts
@@ -11,14 +11,20 @@ export class SinglyLinkedListComponent implements OnInit {
   initialValue: number;
   inputValue: number;
   message: string;
+  max: number;
 
   constructor() {
     this.initialValue = 10;
     this.linkedList = new SinglyLinkedList<number>(this.initialValue);
     this.inputValue = this.initialValue;
+    this.max = 1000;
   }
 
   ngOnInit(): void {
+  }
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
   append(): void {
@@ -26,8 +32,20 @@ export class SinglyLinkedListComponent implements OnInit {
     this.message = `Append '${this.inputValue}'`;
   }
 
+  appendRandom(): void {
+    this.inputValue = this.getRandomInt(this.max);
+    this.linkedList.append(this.inputValue);
+    this.message = `Append Random number '${this.inputValue}' between ${this.max}`;
+  }
+
   prepend(): void {
     this.linkedList.prepend(this.inputValue);
     this.message = `Prepend '${this.inputValue}'`;
+  }
+
+  prependRandom(): void {
+    this.inputValue = this.getRandomInt(this.max);
+    this.linkedList.prepend(this.inputValue);
+    this.message = `Prepend Random number '${this.inputValue}' between ${this.max}`;
   }
 }
