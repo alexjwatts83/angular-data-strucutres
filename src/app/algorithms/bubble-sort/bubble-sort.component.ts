9c0d26e6270b6e4718a5dbf25efa5d28063ef121
alexwatts83 <alexjwatts83@gmail.com>
2020-03-30 08:23:44 +1100

    wip bubble sort

--- src/app/algorithms/bubble-sort/bubble-sort.component.ts
+++ src/app/algorithms/bubble-sort/bubble-sort.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BubbleSort } from '.';
 
 @Component({
   selector: 'app-bubble-sort',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bubble-sort.component.scss']
 })
 export class BubbleSortComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  sort: BubbleSort<number>;
+  inputValue: string;
+  unsortedArray: number[];
+  sortedArray: number[];
+  sortSequence: string[];
+  constructor() { 
+    this.inputValue = '99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0';
+    this.sort = new BubbleSort();
+    this.unsortedArray = [];
+    this.sortedArray = [];
+    this.sortSequence = ['Hello'];
   }
 
+  ngOnInit(): void {
+    this.sortArray();
+  }
+
+  sortArray(): void {
+    const spacesRemoved = this.inputValue.replace(' ', '');
+    const unsortedArrayAsString = spacesRemoved.split(',');
+    this.unsortedArray = [];
+    for(let i = 0; i < unsortedArrayAsString.length; i++) {
+      this.unsortedArray.push(parseInt(unsortedArrayAsString[i]));
+    }
+    this.sortedArray = this.sort.sort(this.unsortedArray)
+  }
 }
--- src/app/algorithms/bubble-sort/bubble-sort.model.ts
+++ src/app/algorithms/bubble-sort/bubble-sort.model.ts
@@ -1,3 +1,35 @@
 export class BubbleSort<T> {
-  
+  sort(items: T[]): T[]{
+    if (items.length <= 1) {
+      return items;
+    }
+
+    let sorted = [...items];
+    let index = 0;
+    let maxIndex = items.length - 1;
+    let iterationCount = 0;
+    let maxLoop = items.length - 1;
+    while(iterationCount < maxIndex){
+      console.log('===============================');
+      console.log(`iterationCount: ${iterationCount}`);
+      while(index < maxLoop) {
+        console.log(`index: ${index};maxLoop: ${maxLoop}`);
+        let n1 = items[index];
+        let n2 = items[index+1];
+        console.log(`n1: ${n1};n2: ${n2}`);
+        if (n2 > n1) {
+          items[index] = n2;
+          items[index+1] = n1;
+          console.log('swapping');
+        }
+        index++;
+      }
+      index = 0;
+      iterationCount++;
+      maxLoop--;
+      console.log('===============================');
+    }
+
+    return sorted;
+  }
 }
