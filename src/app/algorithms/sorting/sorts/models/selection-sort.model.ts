8a7bf367787dfb76c7c2e69bcc155c4bf771446d
alexwatts83 <alexjwatts83@gmail.com>
2020-03-30 10:28:46 +1100

    selection sort done

--- src/app/algorithms/sorting/sorts/models/selection-sort.model.ts
+++ src/app/algorithms/sorting/sorts/models/selection-sort.model.ts
@@ -7,6 +7,26 @@ export class SelectionSort<T> implements SortAlgorithm<T> {
     }
 
     let sorted = [...items];
+    let itemsLength = sorted.length;
+    let index = 0;
+    let isSorted = false;
+    while(!isSorted) {
+      isSorted = true;
+      while(index < itemsLength -1){
+        let firstItem = sorted[index];
+        let secondtItem = sorted[index + 1];
+        // console.log(`sorted[${innerLoop}]:${firstItem};sorted[${innerLoop+1}]:${secondtItem}`);
+        if (firstItem > secondtItem) {
+          console.log(` swap ${firstItem} for ${secondtItem} at index ${index}`);
+          sorted[index] = secondtItem;
+          sorted[index + 1] = firstItem;
+          isSorted = false;
+        }
+        index++;
+      }
+      index = 0
+      console.log(isSorted, JSON.stringify(sorted));
+    }
 
     return sorted;
   }
