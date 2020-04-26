c3b54b23db6ab251ffcb1f8023959925dd14fd63
alexwatts83 <alexjwatts83@gmail.com>
2020-04-26 16:40:28 +1000

    updated the hash function for hash functions

--- src/app/hashes/hashes.model.ts
+++ src/app/hashes/hashes.model.ts
@@ -8,12 +8,15 @@ export class HasTable{
     console.log(this.data);
   }
 
-  _hash(key: string): number {
-    let hash = 0;
-    for (let i =0; i < key.length; i++){
-        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+  _hash(key: string) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96
+      total = (total * WEIRD_PRIME + value) % this.size;
     }
-    return hash;
+    return total;
   }
 
   set(key: string, value: any): void{
